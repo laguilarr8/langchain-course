b9776bdd0925dadb94091ced84074756c89436e2
Luis Carlos <laguilar.r8@gmail.com>
2026-04-23 12:28:57 -0500

    react-search-agent

--- main.py
+++ main.py
@@ -6,55 +6,39 @@ from dotenv import load_dotenv
 os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
-# 2. 🔥 CAMBIO CLAVE: Forzar la lectura del archivo .env actual
+# 2. Forzar la lectura del archivo .env actual
 load_dotenv(override=True)
 
-from langchain_core.prompts import PromptTemplate
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langsmith import wrappers
 
-def main():
-    # Pequeño chequeo de seguridad
-    api_key = os.environ.get("GOOGLE_API_KEY")
-    if not api_key:
-        print("❌ ERROR: No se encontró ninguna clave en el archivo .env")
-        return
-    elif api_key.endswith("Lk1Q"):
-        print("⚠️ ALERTA: ¡Tu código sigue leyendo la clave vieja y bloqueada!")
-        print("Asegúrate de haber guardado el archivo .env con la clave nueva.")
-        return
-
-    information = "Nicolas Maquiavelo ..."
-
-    summary_template = """
-    dada la información {information} sobre una persona, quiero que crees:
-    1. Un resumen corto
-    2. dos datos interesantes sobre ella
-    3. edad y patrimonio
-    4. mejores frases y libros
+@tool
+def search(query: str) -> str:
     """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"],
-        template=summary_template
-    )
+# Usamos el modelo estándar
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+tool = [search]
+agent = create_agent(model=llm, tools=tool)
 
-    # 3. Usamos el modelo estándar
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",  # gemini-2.5-flash 
-        temperature=0,
-        request_timeout=30  
-    )
 
-    chain = summary_prompt_template | llm
-
-    try:
-        response = chain.invoke({"information": information})
-        print("\n=== RESPUESTA DE GEMINI ===")
-        print(response.content)
-    except Exception as e:
-        print(f"\nOcurrió un error al conectar con la API: {e}")
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
+    print(result)
+    
 
 if __name__ == "__main__":
     main()
